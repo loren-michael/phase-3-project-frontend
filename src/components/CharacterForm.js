323b968927aa6f55dc8e635a31b780a34cb50f04
loren <lorenmichaelh@gmail.com>
2022-10-10 10:59:22 -0500

    new character form progression

--- src/components/CharacterForm.js
+++ src/components/CharacterForm.js
@@ -6,44 +6,56 @@ function CharacterForm({ currentUser, handleAddCharacter }) {
     const userId = currentUser.id
     const [newCharacter, setNewCharacter] = useState({
         name: "",
-        race: "",
+        race: "Dwarf",
+        character_class: "Fighter",
         level: 1,
+        icon: "https://styles.redditmedia.com/t5_2vbgl/styles/communityIcon_c329v6c343791.png",
         game_id: 1,
         user_id: userId
     });
 
-    function handleNewChar(e) {
-        e.preventDefault();
-    }
-
     function handleClassChange(e) {
         const selection = e.target.value;
-        setNewCharacter({character_class: e.target.value});
+        setNewCharacter({...newCharacter, character_class: e.target.value});
         if (selection === "Barbarian") {
-            setNewCharacter({icon: "https://www.dndbeyond.com/avatars/10/0/636336416778392507.jpeg"})
+            setNewCharacter({...newCharacter, icon: "https://www.dndbeyond.com/avatars/10/0/636336416778392507.jpeg"})
         } else if (selection === "Bard") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/1/636336416923635770.jpeg"})
         } else if (selection === "Cleric") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/2/636336417054144618.jpeg"})
         } else if (selection === "Druid") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/3/636336417152216156.jpeg"})
         } else if (selection === "Fighter") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/4/636336417268495752.jpeg"})
         } else if (selection === "Monk") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/5/636336417372349522.jpeg"})
         } else if (selection === "Paladin") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/6/636336417477714942.jpeg"})
         } else if (selection === "Ranger") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/7/636336417569697438.jpeg"})
         } else if (selection === "Rogue") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/8/636336417681318097.jpeg"})
         } else if (selection === "Sorcerer") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/9/636336417773983369.jpeg"})
         } else if (selection === "Warlock") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/12/636336422983071263.jpeg"})
         } else if (selection === "Wizard") {
-            setNewCharacter({icon: ""})
+            setNewCharacter({...newCharacter,icon: "https://www.dndbeyond.com/avatars/10/11/636336418370446635.jpeg"})
         }
+    }
+
+    function handleNewChar(e) {
+        e.preventDefault();
+
+        fetch(`http://localhost:9292/characters`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newCharacter)
+        })
+        .then(navigate(`/${userId}/characters`))
+        // .then(handleAddCharacter())
     }
 
     return (
@@ -52,11 +64,10 @@ function CharacterForm({ currentUser, handleAddCharacter }) {
             <h4>Create a new character</h4>
             <form onSubmit={handleNewChar}>
                 <label>Character Name: </label>
-                <input type="text" value={newCharacter.name} onChange={e => setNewCharacter({...newCharacter, name: e.target.value})} />
+                <input type="text" name="name" value={newCharacter.name} onChange={e => setNewCharacter({...newCharacter, name: e.target.value})} />
                 <br></br>
                 <label>Race (Choose one): </label>
-                <select value={newCharacter.race} onChange={e => setNewCharacter({race: e.target.value})}>
-                    <option></option>
+                <select name="race" value={newCharacter.race} onChange={e => setNewCharacter({race: e.target.value})}>
                     <option>Dwarf</option>
                     <option>Dragonborn</option>
                     <option>Elf</option>
@@ -67,8 +78,7 @@ function CharacterForm({ currentUser, handleAddCharacter }) {
                 </select>
                 <br></br>
                 <label>Class (Choose one): </label>
-                <select value={newCharacter.character_class} onChange={handleClassChange}>
-                    <option></option>
+                <select name="character-class" value={newCharacter.character_class} onChange={handleClassChange}>
                     <option>Barbarian</option>
                     <option>Bard</option>
                     <option>Cleric</option>
@@ -82,9 +92,33 @@ function CharacterForm({ currentUser, handleAddCharacter }) {
                     <option>Warlock</option>
                     <option>Wizard</option>
                 </select>
+                <br></br>
+                <label>Level: </label>
+                <select name="level" value={newCharacter.level} onChange={e => setNewCharacter({...newCharacter, level: e.target.value})} >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                    <option>13</option>
+                    <option>14</option>
+                    <option>15</option>
+                    <option>16</option>
+                    <option>17</option>
+                    <option>18</option>
+                    <option>19</option>
+                    <option>20</option>
+                </select>
             </form>
             <br></br>
-            <button type="Submit">Submit New Character</button>
+            <button type="Submit" onSubmit={handleNewChar}>Submit New Character</button>
         </div>
     )
 };
